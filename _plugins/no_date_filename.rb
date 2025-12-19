1a6954744c249a547d4e4f11b5bcee3394a07b16
Biju Punalor <biju.punalor@gmail.com>
2025-12-19 08:36:31 +0530

    jekyll plugin to removes requirement for date from post filenames

---
+++ _plugins/no_date_filename.rb
@@ -0,0 +1,8 @@
+# _plugins/no_date_filename.rb
+module Jekyll
+  class PostReader
+    def read_posts(dir)
+      read_publishable(dir, "_posts", Document::DATELESS_FILENAME_MATCHER)
+    end
+  end
+end
